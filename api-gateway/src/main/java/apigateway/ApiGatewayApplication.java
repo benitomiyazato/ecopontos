552e3ef57a09d289b6d5258b635b85c3be7a278e
bemiyaz <benito.miya172@gmail.com>
2023-05-29 17:28:46 -0300

    AuthenticationGatewayFilterFactory

--- api-gateway/src/main/java/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "apigateway.filters")
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
---
+++ api-gateway/src/main/java/apigateway/filters/AuthenticationGatewayFilterFactory.java
@@ -0,0 +1,77 @@
+package apigateway.filters;
+
+import org.apache.http.HttpHeaders;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthenticationGatewayFilterFactory extends AbstractGatewayFilterFactory<AuthenticationGatewayFilterFactory.Config> {
+
+    public AuthenticationGatewayFilterFactory(WebClient.Builder webClient) {
+        super(Config.class);
+        System.out.println("CONSTRUINDO ESSA PORRA");
+        this.webClient = webClient;
+    }
+
+    private final WebClient.Builder webClient;
+
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+            String[] parts = authHeader.split(" ");
+
+            if (parts.length != 2 || !"Bearer".equals(parts[0])) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            try {
+                webClient.build()
+                        .get()
+                        .uri("http://authentication-service/api/auth/validate?token=" + parts[1])
+                        .retrieve()
+                        .bodyToMono(Void.class)
+                        .subscribe();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return onError(exchange, "Invalid Token", HttpStatus.UNAUTHORIZED);
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//        return (exchange, chain) -> {
+//            ServerHttpResponse response = exchange.getResponse();
+//            response.setStatusCode(HttpStatus.OK);
+//            response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+//            return response.writeWith(Mono.just(response.bufferFactory().wrap("Filter Applied".getBytes())))
+//                    .then(chain.filter(exchange));
+//        };
+//    }
+
+
+    private Mono<Void> onError(ServerWebExchange exchange, String error, HttpStatus status) {
+        ServerHttpResponse response = exchange.getResponse();
+
+        response.setStatusCode(status);
+        return response.setComplete();
+    }
+
+    public static class Config {
+    }
+}
